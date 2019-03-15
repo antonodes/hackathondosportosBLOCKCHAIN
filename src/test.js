d86c6418bd4187cc0bf6f0eff24b847c35acd871
anton <antonio@unisantos.br>
2019-03-15 18:42:24 -0300

    Create function getLastBlock

--- src/test.js
+++ src/test.js
@@ -6,4 +6,4 @@ const conteiner = new Blockchain();
 conteiner.createNewBlock(2389, '00INANSFDFDFDF', 'FDFDFSD5F4SD45DFS');
 conteiner.createNewBlock(111, '00INANSFDFDFDfgdfgdfF', 'FDFDFSD5F4SD45DFSfdsfsdfds');
 conteiner.createNewBlock(12081998, '0gdfgdfgdfgf', 'fhgfhfghfgh');
-console.log(conteiner);
+console.log(conteiner);c
