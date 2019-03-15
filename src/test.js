c9e9d38eb8d4f052e135dab4c84e3efc22a80a82
anton <antonio@unisantos.br>
2019-03-15 02:50:17 -0300

    Create Unit test in function createNewBlock

--- src/test.js
+++ src/test.js
@@ -1,1 +1,9 @@
-$null 
+//Import blockchain
+const Blockchain = require('./blockchain')
+
+const conteiner = new Blockchain();
+
+conteiner.createNewBlock(2389, '00INANSFDFDFDF', 'FDFDFSD5F4SD45DFS');
+conteiner.createNewBlock(111, '00INANSFDFDFDfgdfgdfF', 'FDFDFSD5F4SD45DFSfdsfsdfds');
+conteiner.createNewBlock(12081998, '0gdfgdfgdfgf', 'fhgfhfghfgh');
+console.log(conteiner);
