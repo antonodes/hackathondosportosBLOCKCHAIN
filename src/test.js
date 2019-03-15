1e8fc515a1dc02e368ba12ed78608eb9f86ebbe1
anton <antonio@unisantos.br>
2019-03-15 18:47:55 -0300

    Testing getLastBlock

--- src/test.js
+++ src/test.js
@@ -6,4 +6,10 @@ const conteiner = new Blockchain();
 conteiner.createNewBlock(2389, '00INANSFDFDFDF', 'FDFDFSD5F4SD45DFS');
 conteiner.createNewBlock(111, '00INANSFDFDFDfgdfgdfF', 'FDFDFSD5F4SD45DFSfdsfsdfds');
 conteiner.createNewBlock(12081998, '0gdfgdfgdfgf', 'fhgfhfghfgh');
-console.log(conteiner);c
+conteiner.createNewBlock(12081998, '000000000000', 'LastBlock');
+
+console.log(conteiner);
+
+
+console.log("Get last block save");
+console.log(conteiner.getLastBlock());
