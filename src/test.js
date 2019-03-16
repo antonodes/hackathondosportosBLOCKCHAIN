8f57407e77920f42b56a179c506828f686273c2c
anton <antonio@unisantos.br>
2019-03-15 23:57:37 -0300

    Create testing function createHash

--- src/test.js
+++ src/test.js
@@ -1,16 +1,38 @@
 //Import blockchain
 const Blockchain = require('./blockchain')
-
 const container = new Blockchain();
 
-container.createNewBlock(2389, '00INANSFDFDFDF', 'FDFDFSD5F4SD45DFS');
-container.createNewBlock(111, '00INANSFDFDFDfgdfgdfF', 'FDFDFSD5F4SD45DFSfdsfsdfds');
-container.createNewBlock(12081998, '0gdfgdfgdfgf', 'fhgfhfghfgh');
-container.createNewBlock(12081998, '000000000000', 'LastBlock');
+const previousBlockhash = "ANTONIOFLAVIOTESTING";
+const currentBlockData = [
+  {
+    container: 'ABCCONTEINER123',
+    sender: 'JFGKFJGDFG45FGKJ4s',
+    recipient: '4554FDGDFGFDGDF'
+  },
+  {
+    container: 'CDECONTEINER456',
+    sender: 'GFGDFGDF566565',
+    recipient: '56454GDFGDFGDFGD'
+  },
+  {
+    container: 'FGHCONTEINER789',
+    sender: '4545FDGDFGDFGDFFDGF',
+    recipient: '4554FDGFDGDFGDF'
+  }
+];
 
-container.createNewTransaction(111, 'HDFGKJDFHJKGDFHGJKF', 'JEGSGDFKJGDF');
+const nonce = 100;
 
-container.createNewBlock(123123, 'GHJFDGHJFG', 'UDFGDFOGIOFG');
+console.log(container.hashBlock(previousBlockhash, currentBlockData, nonce));
+
+//container.createNewBlock(2389, '00INANSFDFDFDF', 'FDFDFSD5F4SD45DFS');
+//container.createNewBlock(111, '00INANSFDFDFDfgdfgdfF', 'FDFDFSD5F4SD45DFSfdsfsdfds');
+//container.createNewBlock(12081998, '0gdfgdfgdfgf', 'fhgfhfghfgh');
+//container.createNewBlock(12081998, '000000000000', 'LastBlock');
+
+//container.createNewTransaction(111, 'HDFGKJDFHJKGDFHGJKF', 'JEGSGDFKJGDF');
+
+//container.createNewBlock(123123, 'GHJFDGHJFG', 'UDFGDFOGIOFG');
 
 
 console.log(container);
