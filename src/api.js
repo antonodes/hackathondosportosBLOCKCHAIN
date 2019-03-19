029e812b39bd37a5c0510be478870694c5992c6e
anton <antonio@unisantos.br>
2019-03-19 06:53:05 -0300

    testing body reading and post method in postman

--- src/api.js
+++ src/api.js
@@ -1,11 +1,18 @@
 var express = require("express");
 var app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Creation of a new block
 app.get("/blockchain", function(req, res) {});
 
 //Container transaction
-app.post("/transaction", function(req, res) {});
+app.post("/transaction", function(req, res) {
+  console.log(req.body);
+  res.send("Testing Postman");
+});
 
 //Terminal consensus
 app.post("/consensus", function(req, res) {});
