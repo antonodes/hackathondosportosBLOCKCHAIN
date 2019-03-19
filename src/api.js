79fbc04e1f484ac11ab4423bcfe4ab025de85a3e
anton <antonio@unisantos.br>
2019-03-19 07:02:17 -0300

    Update /blockchain

--- src/api.js
+++ src/api.js
@@ -1,12 +1,17 @@
 var express = require("express");
 var app = express();
 const bodyParser = require("body-parser");
+const Blockchain = require("./blockchain");
+
+const container = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //Creation of a new block
-app.get("/blockchain", function(req, res) {});
+app.get("/blockchain", function(req, res) {
+  res.send(container);
+});
 
 //Container transaction
 app.post("/transaction", function(req, res) {
